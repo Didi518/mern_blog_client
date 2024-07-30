f7712a324d0126fb359606def3b766f95a2755fa
Didi518 <kevin.ouali@gmail.com>
2024-07-30 23:08:51 +0200

    :lipstick: CTA section

--- src/constants/images.js
+++ src/constants/images.js
@@ -1,9 +1,11 @@
+import CtaImage from "../assets/CtaImage.jpg";
 import Logo from "../assets/Logo.svg";
 import HeroImage from "../assets/HeroImage.svg";
 import Post1Image from "../assets/posts/post1.jpg";
 import PostProfile from "../assets/posts/post-profile.svg";
 
 const images = {
+  CtaImage,
   Logo,
   HeroImage,
   Post1Image,
