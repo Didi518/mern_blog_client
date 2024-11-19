e6357ad208586dffa1f807136d3624996cc2fd9c
Didi518 <kevin.ouali@gmail.com>
2024-11-19 13:11:18 +0100

    render.com

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import axios from "axios";
 
 import "./index.css";
 import App from "./App";
 import store from "./store";
+
+axios.defaults.baseURL = process.env.REACT_APP_SERVER_URL;
 
 const queryClient = new QueryClient({
   defaultOptions: {
