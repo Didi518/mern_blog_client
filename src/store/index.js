4c926d273240c0bcd48a8ebf431928e8029ac918
Didi518 <kevin.ouali@gmail.com>
2026-05-04 21:03:56 +0200

    maj vercel

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from '@reduxjs/toolkit';
 
-import { userReducer } from "./reducers/userReducers";
+import { userReducer } from './reducers/userReducers';
 
-const userInfoFromStorage = localStorage.getItem("compte")
-  ? JSON.parse(localStorage.getItem("compte"))
+const userInfoFromStorage = localStorage.getItem('compte')
+  ? JSON.parse(localStorage.getItem('compte'))
   : null;
 
 const initialState = {
-  user: { userInfo: userInfoFromStorage },
+  userInfo: userInfoFromStorage,
 };
 
 const store = configureStore({
   reducer: {
     user: userReducer,
   },
-  preloadedState: initialState,
+  preloadedState: {
+    user: initialState,
+  },
 });
 export default store;
